fa3d0cde475c8a1fa706e03d7f0a8f6ef5d1ef53
warrickma <myf1996@gmail.com>
2022-02-01 15:50:33 -0500

    Updated uts

--- script.R
+++ script.R
@@ -4,15 +4,20 @@ library(scales)
 library(svglite)
 library(data.table)
 data = read.csv("data.csv")
-#Conduct t test
-young_t_test = t.test(data[(1:3), 2], data[(4:6), 2])
-strain_t_test = t.test(data[(1:3), 3], data[(4:6), 3])
+young_aov = aov(Young~Sample, data)
+strainatbreak_aov = aov(Strain_at_break~Sample, data)
+uts_aov = aov(UTS~Sample, data)
+summary(young_aov)
+summary(strainatbreak_aov)
+summary(uts_aov)
+TuckeyHSD(young_aov)
 setDT(data)
 str(data)
 dataMelt = melt(data, id.vars = "Sample")
 dataMelt_agg = dataMelt[, .(mean = mean(value), se = sd(value)/.N), by = .(Sample, variable)]
-data_young = dataMelt_agg[(1:2),]
-data_strain = dataMelt_agg[(3:4),]
+data_young = dataMelt_agg[(1:3),]
+data_strain = dataMelt_agg[(4:6),]
+data_uts = dataMelt_agg[(7:9),]
 ggplot(data_young, aes(x = Sample, y = mean)) +
   geom_bar(stat = "identity", position = "dodge") +
   geom_errorbar(aes(ymax = mean + 1.96*se, 
@@ -20,7 +25,7 @@ ggplot(data_young, aes(x = Sample, y = mean)) +
                     width = 0.2),
                 position = position_dodge(0.9)) +
   labs(y = "Young's Modulus (MPa)") +
-  scale_x_discrete(labels=c("25 wt% PPS", "30 wt% PPS")) +
+  scale_x_discrete(labels=c("25 wt% PPS", "30 wt% PPS", "37 wt% PPS")) +
   scale_y_continuous(limits = c(0,12), n.breaks = 5) +
   theme_classic() +
   theme(
@@ -40,7 +45,7 @@ ggplot(data_strain, aes(x = Sample, y = mean)) +
                     width = 0.2),
                 position = position_dodge(0.9)) +
   labs(y = "Strain at Break (mm/mm)") +
-  scale_x_discrete(labels=c("25 wt% PPS", "30 wt% PPS")) +
+  scale_x_discrete(labels=c("25 wt% PPS", "30 wt% PPS", "37 wt% PPS")) +
   scale_y_continuous(limits = c(0,0.70), n.breaks = 10, labels = scales::percent) +
   theme_classic() +
   theme(
@@ -52,3 +57,23 @@ ggplot(data_strain, aes(x = Sample, y = mean)) +
     legend.text = element_text(size=10, color = "black", face = "bold"),
     legend.position = "none")
 ggsave("strain.svg", width = 80, height = 75, units = "mm")
+
+ggplot(data_uts, aes(x = Sample, y = mean)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_errorbar(aes(ymax = mean + 1.96*se, 
+                    ymin = mean - 1.96*se,
+                    width = 0.2),
+                position = position_dodge(0.9)) +
+  labs(y = "Ultimate Tensile Stress (MPa)") +
+  scale_x_discrete(labels=c("25 wt% PPS", "30 wt% PPS", "37 wt% PPS")) +
+  scale_y_continuous(limits = c(0,3.5), n.breaks = 5) +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(size=10, color = "black", face = "bold"),
+    axis.text.y = element_text(size=10, color = "black", face = "bold"),
+    axis.title.x = element_blank(),
+    axis.title.y = element_text(size=10, color = "black", face = "bold"),
+    legend.title = element_text(size=10, color = "black", face = "bold"),
+    legend.text = element_text(size=10, color = "black", face = "bold"),
+    legend.position = "none")
+ggsave("uts.svg", width = 80, height = 75, units = "mm")
